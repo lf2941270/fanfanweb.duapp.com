b69e01fdde9c73f3a83beb78a0655c87fd8e07eb
李凡 <lf2941270@163.com>
2014-01-23 22:56:01 +0800

    1

--- models/conn.js
+++ models/conn.js
@@ -1,14 +1,14 @@
 var settings = require('../settings');
 var mongodb = require('./db');
-module.exports=function(callback2){
+module.exports=function(callback){
     mongodb.open(function(err, mongodb) {
         mongodb.authenticate(settings.user, settings.password, function(err, result) {
             if (err) {
                 mongodb.close();
-                callback2(err)
+                callback(err)
                 return;
             }
-            callback2(null,mongodb);
+            callback(null,mongodb);
         });
     });
 }
