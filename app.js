d57288f494453ea0fdfd0ab916fc6b035c809088
李凡 <lf2941270@163.com>
2014-06-05 11:36:21 +0800

    1

--- app.js
+++ app.js
@@ -39,6 +39,7 @@ app.use(express.logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'ibaiyu.com')));
 app.use(express.session({
     secret: settings.cookieSecret
 }))
