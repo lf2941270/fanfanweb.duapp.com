ae762ba570fdcd9767e100136a5249c5285c8333
李凡 <lf2941270@163.com>
2014-06-12 12:37:30 +0800

    qzone测试

--- qzone/browser/index.js
+++ qzone/browser/index.js
@@ -62,7 +62,7 @@ Browser.prototype.setCookie =function(value){
 		}
     this.cookie[arr.shift().trim()]=arr.join('=').trim();
   }
-  this.headers.cookie=this.headers.cookie||"";
+  this.headers=this.headers||{};
   this.headers.cookie=stringifyCookie(this.cookie);
 }
 /*对响应头进行处理*/
