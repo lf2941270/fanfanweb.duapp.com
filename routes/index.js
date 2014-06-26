40d87bd37038ac6352f49168c8f8bee8c0569d25
李凡 <lf2941270@163.com>
2014-06-26 16:06:16 +0800

    添加头像

--- routes/index.js
+++ routes/index.js
@@ -248,7 +248,7 @@ function beforeAll(req,res,next){
   if(ipWhiteList.indexOf(req._remoteAddress)===-1){
       res.send('<script type="text/javascript">alert("你好，来自'+req._remoteAddress+'的逗比！你暂时没有访问权限哦！")</script>');
   }else{
-    res.set('Access-Control-Allow-Origin', 'http://localhost:63342');
+    res.set('Access-Control-Allow-Origin', 'http://localhost:*');
 
     next();
   }
